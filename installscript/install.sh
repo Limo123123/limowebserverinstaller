b1e279654835133a65227510ab6b1a75ccc3d524
Limo123123 <floriantoens273@gmail.com>
2024-02-16 19:10:36 +0100

    New installskript

--- installscript/install.sh
+++ installscript/install.sh
@@ -1,35 +1,20 @@
-# create a folder to put the files in it
-mkdir installfiles
+#!/bin/bash
 
-cd installfiles
+echo "Downloading Zip from Server..."
+wget --header 'Host: uc9596b72edefd6fec96626040bd.dl.dropboxusercontent.com' --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' --header 'Accept-Language: de,en-US;q=0.7,en;q=0.3' --header 'Accept-Encoding: gzip, deflate, br' --header 'Connection: keep-alive' --header 'Upgrade-Insecure-Requests: 1' --header 'Sec-Fetch-Dest: document' --header 'Sec-Fetch-Mode: navigate' --header 'Sec-Fetch-Site: none' --header 'Sec-Fetch-User: ?1' 'https://uc9596b72edefd6fec96626040bd.dl.dropboxusercontent.com/cd/0/get/CNY1U_PxyoRkALR6O_V6ORZV0wP4CoaSOJUXEG-tldj0Pfg1sEFrM-pul1Heb2UQ3d_Yts5tmuAXSU7gKIYC0l2UI9L30UNLMMVqRSd9SHG17l4KuoRxYfIOSDyQRUjUMLV9FLPgTg3A0BbH3q65362F/file?dl=1#' --output-document 'html.zip'
 
-sudo apt install apache2 -y
-
-sudo chown -R pi:www-data -R /var/www/html
-
-chmod 750 -R /var/www/html
-
-sudo apt install git -y
-
-cd /var/www/html
-
-rm index.html
-
-cd
-
-cd installfiles
-
-sudo apt install wget -y
-
-wget https://www.dropbox.com/scl/fo/ljcl4jbc6gobq8z25bwzl/h?rlkey=zfaif3j52z1ojrqf01zov8cp1&dl=1
+sleep 2
 
 unzip html.zip
 
-cd html
+sudo apt update && sudo apt upgrade -y
 
-cp -R ./html/ /var/www/html 
+sudo apt install apache2 php -y
 
-cd
+sudo chmod -R pi:www-data -R /var/www/html
+chmod 750 -R /var/www/html
 
+cp -r html/ /var/www/html
 
+echo "That was it"
 
