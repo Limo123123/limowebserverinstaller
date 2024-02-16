a2ba0c69f17239885db7b900d533126f5b299da6
Limo123123 <floriantoens273@gmail.com>
2024-02-16 20:39:10 +0100

    Updates.................

--- installscript/install.sh
+++ installscript/install.sh
@@ -4,7 +4,7 @@
 echo "Installing the webserver"
 
 
-wget http://limo/downloads/testing/html.zip
+wget --header 'Host: limo' --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' --header 'Accept-Language: de,en-US;q=0.7,en;q=0.3' --header 'Accept-Encoding: gzip, deflate' --header 'Connection: keep-alive' --header 'Upgrade-Insecure-Requests: 1' 'http://limo/downloads/testing/html.zip' --output-document 'html.zip'
 
 
 unzip html.zip
