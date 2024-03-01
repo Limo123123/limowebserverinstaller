c22cd1f4ec3fe6e66119a3a4b105eb9526c15461
Limo123123 <floriantoens273@gmail.com>
2024-03-01 21:55:37 +0100

    rfg

--- installscript/install.sh
+++ installscript/install.sh
@@ -1,33 +1,13 @@
 #!/bin/bash
 
-
 echo "Installing the webserver"
-
-
 wget http://limo/downloads/service/webserver/v1/html.zip
-
-
 unzip html.zip
-
-
 sudo apt install apache2 -y
-
-
-sudo chmod -R pi:www-data -R /var/www/html
-
-
-chmod 750 -R /var/www/html
-
-
-cp -r html /var/www/html
-
-
+sudo chown -R pi:www-data /var/www/html
+sudo chmod -R 750 /var/www/html
+cp -r html/* /var/www/html
 rm html.zip
-
-
 rm -r html
-
-
 echo "That was it"
 
-# GIthub github github musst be funny
