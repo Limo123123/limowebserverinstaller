1fe0ec2e4817c340c17e0c22ccaf16b0574035f0
Limo123123 <floriantoens273@gmail.com>
2024-03-01 21:40:02 +0100

    new

--- installscript/install.sh
+++ installscript/install.sh
@@ -4,13 +4,13 @@
 echo "Installing the webserver"
 
 
-wget --header 'Host: limo' --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' --header 'Accept-Language: de,en-US;q=0.7,en;q=0.3' --header 'Accept-Encoding: gzip, deflate' --header 'Connection: keep-alive' --header 'Upgrade-Insecure-Requests: 1' 'http://limo/downloads/testing/html.zip' --output-document 'html.zip'
+wget http://limo/downloads/service/webserver/v1/html.zip
 
 
 unzip html.zip
 
 
-sudo apt install apache2 
+sudo apt install apache2 -y
 
 
 sudo chmod -R pi:www-data -R /var/www/html
