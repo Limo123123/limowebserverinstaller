42669b5cdcd3a99b174963491de49280e6159c04
Limo123123 <90343619+Limo123123@users.noreply.github.com>
2024-04-24 17:38:37 +0200

    Update install.sh

--- installscript/install.sh
+++ installscript/install.sh
@@ -4,6 +4,7 @@ echo "Installing the webserver"
 wget https://github.com/Limo123123/limowebserverinstaller/releases/download/2.0/html.zip
 unzip html.zip
 sudo apt install apache2 -y
+sudo apt install php -y
 sudo chown -R pi:www-data /var/www/html
 sudo chmod -R 750 /var/www/html
 cp -r html/* /var/www/html
